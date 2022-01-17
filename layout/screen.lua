87834708fe8ed634db57dc989bcc190d9d55b851
minh-p <minhisverynormal@gmail.com>
2022-01-17 09:39:16 -0800

    increase topbar size

--- layout/screen.lua
+++ layout/screen.lua
@@ -54,7 +54,7 @@ awful.screen.connect_for_each_screen(function(s)
     }
 
     -- Create the wibox
-    s.mywibox = awful.wibar({ position = "top", screen = s })
+    s.mywibox = awful.wibar({ position = "top", screen = s, height = s.geometry.height * 0.02,})
 
     -- Add widgets to the wibox
     s.mywibox:setup {
